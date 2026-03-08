48fbe8831e49a3cc4ec253f82cebf460db33cd0e
szlian <lian.shengzhi.7e9@itb.cat>
2026-03-08 17:58:56 +0100

    Tomogatchi new func added, emotion states updates, a lot of else if added, messages from UI.messages used

--- Tomogatchi/Class/ATomogatchi.cs
+++ Tomogatchi/Class/ATomogatchi.cs
@@ -3,21 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tomogatchi.Class;
+using Tomogatchi.Enums;
+using Tomogatchi.UI;
 
-
-public abstract class ATomogatchi
+namespace Tamagotchi.Class
 {
-    public string Name { get; set; }
-    public string EmotionState { get; set; }
-    public int Stats { get; set; }
-
-    public ATomogatchi(string name, string emotionState, int stats)
+    public abstract class ATamagotchi
     {
-        Name = name;
-        EmotionState = emotionState;
-        Stats = stats;
+        public string Name { get; set; }
+        public EmotionState Emotion { get; set; }
+        public Stats Stats { get; set; }
+
+        protected ATamagotchi(string name)
+        {
+            Name = name;
+            Emotion = EmotionState.Happy;
+            Stats = new Stats();
+        }
+
+        // Actualiza el estado emocional según las estadísticas
+        public void UpdateEmotion()
+        {
+            if (Stats.IsDead)
+            {
+                return;
+            }
+
+            if (Stats.Health <= 20)
+            {
+                Emotion = EmotionState.Sick;
+                Console.WriteLine(Messages.EMOTION_SICK, Name);
+            }
+            else if (Stats.Energy <= 30)
+            {
+                Emotion = EmotionState.Tired;
+                Console.WriteLine(Messages.EMOTION_TIRED, Name);
+            }
+            else if (Stats.Hunger >= 50)
+            {
+                Emotion = EmotionState.Angry;
+                Console.WriteLine(Messages.EMOTION_ANGRY, Name);
+            }
+            else if (Stats.Hunger >= 30)
+            {
+                Emotion = EmotionState.Sad;
+                Console.WriteLine(Messages.EMOTION_SAD, Name);
+            }
+            else
+            {
+                Emotion = EmotionState.Happy;
+                Console.WriteLine(Messages.EMOTION_HAPPY, Name);
+            }
+        }
+
+        public void ShowStatus()
+        {
+            Console.WriteLine("\n--- ESTADO DE " + Name.ToUpper() + " ---");
+            Console.WriteLine(Messages.STATS_EMOTION, Emotion);
+            Stats.ShowStats();
+        }
+
+        // Método abstracto para hacer sonido (cada mascota lo implementa diferente)
+        public abstract void MakeSound();
     }
 }
- 
 
 
