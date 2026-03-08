0182838a9c1ca40ee76f2a6b8a1a706634cae152
szlian <lian.shengzhi.7e9@itb.cat>
2026-03-08 18:15:34 +0100

    Class Pets, (CChicks, CCat, CDog) messages and behaviors added

--- Tomogatchi/obj/Debug/net8.0/Tomogatchi.AssemblyInfo.cs
+++ Tomogatchi/obj/Debug/net8.0/Tomogatchi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tomogatchi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f615c0a4cb574cd45755212586e7d0872f25774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48fbe8831e49a3cc4ec253f82cebf460db33cd0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tomogatchi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tomogatchi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
